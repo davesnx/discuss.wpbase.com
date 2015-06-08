0e0e9ea6776e8103dafcf44f75a800d623e17ec2
David Sancho <dsnxmoreno@gmail.com>
2015-06-09 01:12:00 +0200

    First commit

---
+++ public/src/client/account/header.js
@@ -0,0 +1,28 @@
+define('forum/account/header', function() {
+	var	AccountHeader = {};
+
+	AccountHeader.init = function() {
+		displayAccountMenus();
+		selectActivePill();
+	};
+
+	function displayAccountMenus() {
+		if (!app.user.uid || app.user.uid !== parseInt(ajaxify.variables.get('theirid'), 10)) {
+			$('.account-sub-links .plugin-link.private').each(function() {
+				$(this).addClass('hide');
+			});
+		}
+	}
+
+	function selectActivePill() {
+		$('.account-sub-links li').removeClass('active').each(function() {
+			var href = $(this).find('a').attr('href');
+			if (window.location.href.indexOf(href) !== -1) {
+				$(this).addClass('active');
+				return false;
+			}
+		});
+	}
+
+	return AccountHeader;
+});
